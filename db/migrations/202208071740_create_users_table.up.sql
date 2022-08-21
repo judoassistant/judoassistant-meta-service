723fc4a9950829a2c2d9fb5b9f32f9dfc6d7563e
Svend Christian Svendsen <svendcs@svendcs.com>
2022-08-21 16:56:29 +0200

    Remove unique constraints on users table

--- db/migrations/202208071740_create_users_table.up.sql
+++ db/migrations/202208071740_create_users_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
    id INTEGER PRIMARY KEY,
    email TEXT UNIQUE NOT NULL,
-   first_name TEXT UNIQUE NOT NULL,
-   last_name TEXT UNIQUE NOT NULL,
+   first_name TEXT NOT NULL,
+   last_name TEXT NOT NULL,
    password_hash TEXT NOT NULL,
    is_admin INTEGER NOT NULL
 );
