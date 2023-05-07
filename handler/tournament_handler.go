af26faee9ef0f5154636fbc98186774f293748e7
Svend Christian Svendsen <svendcs@svendcs.com>
2023-05-07 13:09:08 +0200

    Add zap logger to tournament handler

--- handler/tournament_handler.go
+++ handler/tournament_handler.go
@@ -35,12 +35,14 @@ func NewTournamentHandler(tournamentService service.TournamentService, logger *z
 func (handler *tournamentHandler) Index(c *gin.Context) {
 	queryParams := dto.TournamentIndexQueryDTO{}
 	if err := c.ShouldBindQuery(&queryParams); err != nil {
+		handler.logger.Info("Unable to map tournament index query", zap.Error(err))
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
 
 	tournaments, err := handler.tournamentService.Get(queryParams.After, 10)
 	if err != nil {
+		handler.logger.Warn("Unable to get tournament", zap.Error(err))
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
@@ -51,6 +53,7 @@ func (handler *tournamentHandler) Index(c *gin.Context) {
 func (handler *tournamentHandler) GetPast(c *gin.Context) {
 	tournaments, err := handler.tournamentService.GetPast(10)
 	if err != nil {
+		handler.logger.Warn("Unable to get past tournaments", zap.Error(err))
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
@@ -61,6 +64,7 @@ func (handler *tournamentHandler) GetPast(c *gin.Context) {
 func (handler *tournamentHandler) GetUpcoming(c *gin.Context) {
 	tournaments, err := handler.tournamentService.GetUpcoming(10)
 	if err != nil {
+		handler.logger.Warn("Unable to get upcoming tournaments", zap.Error(err))
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
@@ -71,6 +75,7 @@ func (handler *tournamentHandler) GetUpcoming(c *gin.Context) {
 func (handler *tournamentHandler) Create(c *gin.Context) {
 	request := dto.TournamentCreationRequestDTO{}
 	if err := c.BindJSON(&request); err != nil {
+		handler.logger.Info("Unable map create tournament request", zap.Error(err))
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
@@ -79,6 +84,7 @@ func (handler *tournamentHandler) Create(c *gin.Context) {
 
 	response, err := handler.tournamentService.Create(user, &request)
 	if err != nil {
+		handler.logger.Warn("Unable to create tournament", zap.Error(err))
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
@@ -89,12 +95,14 @@ func (handler *tournamentHandler) Create(c *gin.Context) {
 func (handler *tournamentHandler) Get(c *gin.Context) {
 	query := dto.TournamentQueryDTO{}
 	if err := c.ShouldBindQuery(&query); err != nil {
+		handler.logger.Info("Unable map get tournament request", zap.Error(err))
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
 
 	tournament, err := handler.tournamentService.GetById(query.ID)
 	if err != nil {
+		handler.logger.Warn("Unable to get tournament", zap.Error(err))
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
@@ -105,12 +113,14 @@ func (handler *tournamentHandler) Get(c *gin.Context) {
 func (handler *tournamentHandler) Update(c *gin.Context) {
 	query := dto.TournamentQueryDTO{}
 	if err := c.ShouldBindQuery(&query); err != nil {
+		handler.logger.Info("Unable to map query tournament request", zap.Error(err))
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
 
 	request := dto.TournamentUpdateRequestDTO{}
 	if err := c.ShouldBindJSON(&request); err != nil {
+		handler.logger.Info("Unable to map update tournament request", zap.Error(err))
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
@@ -118,6 +128,7 @@ func (handler *tournamentHandler) Update(c *gin.Context) {
 	// Todo: Verify ownership
 	tournament, err := handler.tournamentService.Update(query.ID, &request)
 	if err != nil {
+		handler.logger.Warn("Unable to update tournament", zap.Error(err))
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
