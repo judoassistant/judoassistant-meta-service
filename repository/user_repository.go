a0bc110febd7850bf25751f00603819f72ee6969
Svend Christian Svendsen <svendcs@svendcs.com>
2023-05-07 11:48:55 +0200

    Wrap repository layer errors

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"github.com/jmoiron/sqlx"
 	"github.com/judoassistant/judoassistant-meta-service/entity"
+	"github.com/pkg/errors"
 )
 
 type UserRepository struct {
@@ -14,34 +15,55 @@ func NewUserRepository(db *sqlx.DB) *UserRepository {
 }
 
 func (repository *UserRepository) Create(entity *entity.UserEntity) error {
-	return repository.db.Get(&entity.ID, "INSERT INTO users (first_name, last_name, email, password_hash, is_admin) VALUES ($1, $2, $3, $4, $5) RETURNING id", entity.FirstName, entity.LastName, entity.Email, entity.PasswordHash, entity.IsAdmin)
+	err := repository.db.Get(&entity.ID, "INSERT INTO users (first_name, last_name, email, password_hash, is_admin) VALUES ($1, $2, $3, $4, $5) RETURNING id", entity.FirstName, entity.LastName, entity.Email, entity.PasswordHash, entity.IsAdmin)
+	if err != nil {
+		return errors.Wrap(err, "unable to create user")
+	}
+	return nil
 }
 
 func (repository *UserRepository) Update(entity *entity.UserEntity) error {
 	_, err := repository.db.Exec("UPDATE users SET first_name = $2, last_name = $3, email = $4, password_hash = $5, is_admin = $6 WHERE id = $1", entity.ID, entity.FirstName, entity.LastName, entity.Email, entity.PasswordHash, entity.IsAdmin)
-	return err
+	if err != nil {
+		return errors.Wrap(err, "unable to update user")
+	}
+	return nil
 }
 
 func (repository *UserRepository) ExistsByEmail(email string) (bool, error) {
 	var count int64
 	err := repository.db.Get(&count, "SELECT count(*) FROM users WHERE email = $1 LIMIT 1", email)
-	return count > 0, err
+	if err != nil {
+		return false, errors.Wrap(err, "unable to get user")
+	}
+
+	return count > 0, nil
 }
 
 func (repository *UserRepository) GetById(id int64) (*entity.UserEntity, error) {
 	user := entity.UserEntity{}
 	err := repository.db.Get(&user, "SELECT * FROM users WHERE id = $1 LIMIT 1", id)
-	return &user, err
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get user")
+	}
+
+	return &user, nil
 }
 
 func (repository *UserRepository) GetByEmail(email string) (*entity.UserEntity, error) {
 	user := entity.UserEntity{}
 	err := repository.db.Get(&user, "SELECT * FROM users WHERE email = $1 LIMIT 1", email)
-	return &user, err
+	if err != nil {
+		return nil, errors.Wrap(err, "unable get get user")
+	}
+	return &user, nil
 }
 
 func (repository *UserRepository) GetAll() ([]entity.UserEntity, error) {
 	users := []entity.UserEntity{}
 	err := repository.db.Select(&users, "SELECT * FROM users")
-	return users, err
+	if err != nil {
+		return nil, errors.Wrap(err, "unable get get users")
+	}
+	return users, nil
 }
